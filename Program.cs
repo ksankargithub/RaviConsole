5935c6df74c7dc177166176533c15c0084dc0a37
ksankargithub <sankaranand.katreddi@gmail.com>
2021-06-21 16:40:21 +0530

    Sankarconsolecommit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SankarConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Sankar");
+        }
+    }
+}
